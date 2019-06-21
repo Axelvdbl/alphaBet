0daa94e1349314be0b4026e224146002cb90a7ff
Axel <axel.vandenabeele@epitech.eu>
2019-06-21 15:37:26 +0200

    [feat] : heroku setup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+	letters = [];
 
+	date: any;
+	monthNames = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet',
+	'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
+  constructor(
+		public alertCtrl: AlertController,
+		public data: DataService,
+	) {}
+
+	ngOnInit() {
+		this.getLetters() ;
+		var fullDate = new Date();
+		var year = fullDate.getFullYear();
+		var day = fullDate.getDate();
+		this.date = day+" "+(this.monthNames[fullDate.getMonth()])+" "+year;
+	}
+
+	async pressed(letter, i) {
+		let alert = await this.alertCtrl.create({
+      message: "Valider la lettre " + letter.name.toUpperCase() + " ?",
+			buttons: [
+			 {
+				 text: 'ANNULER',
+				 role: 'cancel'
+			 }, {
+				 text: 'OK',
+				 handler: () => {
+					 letter.count++;
+					 if (!letter.date)
+					 	letter.date = this.date;
+					 this.putLetter(letter, i);
+				 }
+			 }
+		 ]
+    });
+    await alert.present();
+	}
+
+	getLetters() {
+		this.data.getLetters()
+							.subscribe(
+								success => {
+									this.letters = success.body;
+								},
+								error => {
+									console.log(error);
+								}
+							)
+	}
+
+	putLetter(letter, i) {
+		this.data.putLetter(letter)
+							.subscribe(
+								success => {
+								},
+								error => {
+									console.log(error);
+								}
+							)
+	}
 }
